56bcec289cb9a8b0de85a88b00a075483abdab50
Taylor Murphy <tjnpmurphy@gmail.com>
2025-04-02 20:59:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace MultiplicationApplication;
+
+public class MainProgram
+{
+    public class MultiplicationApplication()
+    {
+        public static void Main(string[] args)
+        {
+            // Variable definitions
+            string StartMessage = "Welcome!";
+            string AppName = "-*-MultiplicationMadness-*-";
+            string[] Incorrect = ["Unlucky, you can do the next one!", "Don't be too down, you got this!", "Worry not, practice makes perfect!", "Incorrect, don't worry though, you can get the next one!"];
+            string[] Correct = ["Great Job!", "Keep up the good work!", "Spectacular Work!", "Amazing work , keep it up!"];
+            
+            // Random Number Generator for Array messages
+            Random randomNumberOne = new Random();
+            int randomString = randomNumberOne.Next(-1, 5);
+
+            Console.WriteLine(AppName);
+            // Console.WriteLine(Incorrect[randomString]);
+
+            // Game Boolean
+            bool ApplicationRunning = true;
+
+            while (ApplicationRunning)
+            {
+                Console.WriteLine("Do you want to start the game? (Y/N)");
+                string UserDecision = Console.ReadLine();
+                
+                if (UserDecision.ToUpper() == "Y")
+                {
+                    // Start Message with basic instruction
+                    Console.WriteLine(StartMessage);
+                    Thread.Sleep(300);
+
+                    // Incremental Number Variable
+                    int i = 0;
+                    int Score = 0;
+
+                    while (i < 10)
+                    {
+                        // Rnd Number variable
+                        int NumOne = randomNumberOne.Next(1, 11);
+                        int NumTwo = randomNumberOne.Next(1, 11);
+
+                        // Answer definition
+                        int Answer = NumOne * NumTwo;
+
+                        Thread.Sleep(10);
+                        Console.Clear();
+
+                        // Score Keeper
+                        Console.WriteLine($"Current score: {Score}");
+                        // Question definition
+                        string Question = $"What is {NumOne} x {NumTwo}?";
+
+                        // Printing to console
+                        Console.WriteLine(Question);
+
+                        // User Input
+                        int UserAnswer = Convert.ToInt32(Console.ReadLine());
+
+                        if (UserAnswer != Answer)
+                        {
+                            Console.WriteLine(Incorrect[randomString]);
+                        }
+                        else if (UserAnswer == Answer)
+                        {
+                            Console.WriteLine(Correct[randomString]);
+                            Score++;
+                        }
+                        i++;
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                    }
+                    Console.WriteLine($"Well Done! You got {Score}/{i} correct!");
+
+                }
+                else if(UserDecision.ToUpper() == "N")
+                {
+                    Console.Clear();
+                    ApplicationRunning = false;
+                } else
+                {
+                    Console.WriteLine("Incorrect data entry, please enter Y or N.");
+                }
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
+            ApplicationRunning = false;
+            Environment.Exit(0);
+        }
+    }
+}
